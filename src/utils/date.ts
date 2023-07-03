501074d6dd5e731336b5f334854fb9a2ca85f22c
baoyuhao <1322048907@njupt.edu.cn>
2023-07-03 11:07:57 +0800

    补充一个常用方法

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -23,3 +23,22 @@ export const chargeTheme = () => {
    */
   return hours >= 7 && hours <= 18 ? "light" : "dark";
 };
+
+/**
+ * 获取 n 年后的日期
+ * eq: 2023-07-03 getNYearLaterDate(1) => 2023-07-02
+ * @param nYears
+ */
+export const getNYearLaterDate = (nYears) => {
+  let currentTimeStamp = Date.now();
+  // 计算一年后的时间戳
+  let oneYearLaterTimeStamp = currentTimeStamp + ((nYears - 1) * 365 + 364) * 24 * 60 * 60 * 1000;
+  // 创建一个新的Date对象，使用n年后的时间戳
+  let nYearLaterDate = new Date(oneYearLaterTimeStamp);
+  // 获取年、月、日
+  let year = nYearLaterDate.getFullYear();
+  let month = String(nYearLaterDate.getMonth() + 1).padStart(2, "0");
+  let day = String(nYearLaterDate.getDate()).padStart(2, "0");
+  // 格式化为 "yyyy-MM-dd" 的字符串
+  return year + "-" + month + "-" + day;
+};
